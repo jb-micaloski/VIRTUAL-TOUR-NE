b96ae50718c3e289032a62c0a093a272e1891f1d
jb-micaloski <jb.micaloski2709@gmail.com>
2023-06-24 16:48:29 -0300

    first commit

---
+++ nebrasil_tour/src/components/Pano.jsx
@@ -0,0 +1,47 @@
+import * as PANOLENS from "panolens";
+import * as THREE from "three";
+import React from "react";
+import panoramasData from "../data/Data";
+
+const Pano = () => {
+  const viewer = new PANOLENS.Viewer({
+    container: document.querySelector("#container"),
+    output: 'console',
+    cameraFov: '105',
+    speedLimit: '0.5',
+  });
+
+  const StartPosition = new THREE.Vector3(-4640.72, -67.58, 1836.12)
+
+  const panoramas = panoramasData.map(data => {
+    const panorama = new PANOLENS.ImagePanorama(data.url);
+    panorama.addEventListener( 'enter-fade-start', function(){
+      viewer.tweenControlCenter( StartPosition, 0 );
+    }); // ACRESCENTAR LOOKATPOSITION ON DATA.JS
+  
+    data.infospots.forEach((infospotData, index) => {
+      const position = infospotData.position;
+      const infospot = new PANOLENS.Infospot(500, PANOLENS.DataImage.Arrow);
+      infospot.position.set(position[0], position[1], position[2]);
+      infospot.addHoverText(infospotData.title);
+      infospot.panoramaIndex = infospotData.index; // Adiciona a informação do índice do infospot no panorama
+      panorama.add(infospot);
+      infospot.addEventListener('click', function() {
+        viewer.setPanorama(panoramas[infospot.panoramaIndex]);
+      }
+      );  
+    });
+  
+    return panorama;
+  });
+
+  panoramas.forEach(panorama => {
+    viewer.add(panorama);
+  });
+
+  return (
+    <div id="container"/>
+  );
+};
+
+export default Pano;
---
+++ nebrasil_tour/src/data/Data.js
@@ -0,0 +1,163 @@
+const panoramasData = [
+  //INICIO 
+  {                                                        //0
+    url: "../images/comp/ne-beg-1.jpg",
+    infospots: [
+      {
+        position: [-4534.26, 593.08, 2011.69],
+        title: "INICIAR TOUR",
+        index: 1,
+      }
+    ]
+  },
+  //CONVOO
+  {                                                        //1
+    url: "../images/comp/ne-conv-1.jpg",
+    infospots: [
+      {
+        position: [-4421.00, -281.67, 2297.30],
+        title: "Sala de Estar dos Guardas-Marinha",
+        index: 2,
+      },
+      {
+        position: [4969.53, 467.65, -117.02],
+        title: "VOLTAR",
+        index: 0,
+      }
+    ]
+  },
+  //SALA DE ESTAR DOS GUARDA MARINHA
+  {                                                         //2
+    url: "../images/comp/ne-gm-1.jpg",
+    infospots: [
+      {
+        position: [-4990.44, -65.03, -133.62],
+        title: "Sala de Estar dos Guardas-Marinha",
+        index: 3,
+      },
+      {
+        position: [4971.85, -49.09, -500.27],
+        title: "Convoo do NE Brasil",
+        index: 1,
+      }
+    ]
+  },
+  {                                                          //3
+    url: "../images/comp/ne-gm-2.jpg",
+    infospots: [
+      {
+        position: [-4937.25, -646.49, 329.09],
+        title: "Sala de Estar dos Guardas-Marinha",
+        index: 4,
+      },
+      {
+        position: [-4695.19, -668.81, 1567.48],
+        title: "Sala de Estar dos Guardas-Marinha",
+        index: 5,
+      },
+      {
+        position: [4971.85, -49.09, -500.27],
+        title: "Convoo do NE Brasil",
+        index: 1,
+      }
+    ]
+  },
+  {                                                           //4
+    url: "../images/comp/ne-gm-3.jpg",
+    infospots: [
+      {
+        position: [-4074.28, -1480.28, 2478.77],
+        title: "Sala de Estar dos Guardas-Marinha",
+        index: 5,
+      },
+      {
+        position: [-4642.69, -86.55, -1828.91],
+        title: "Sala de Estar dos Guardas-Marinha",
+        index: 6,
+      },
+      {
+        position: [3976.52, -596.33, 2960.82],
+        title: "Sala de Estar dos Guardas-Marinha",
+        index: 3,
+      }
+    ]
+  },
+  {                                                            //5
+    url: "../images/comp/ne-gm-4.jpg",
+    infospots: [
+      {
+        position: [-4118.23, 206.05, -2811.45],
+        title: "Sala de Estar dos Guardas-Marinha",
+        index: 6,
+      },
+      {
+        position: [-4900.01, -428.24, -855.47],
+        title: "Sala de Estar dos Guardas-Marinha",
+        index: 7,
+      },
+      {
+        position: [-4853.89, -346.92, 1124.48],
+        title: "Sala de Estar dos Guardas-Marinha",
+        index: 8,
+      },
+      {
+        position: [-502.12, 568.16, -4937.46],
+        title: "Sala de Estar dos Guardas-Marinha",
+        index: 4,
+      },
+      {
+        position: [4542.90, -673.13, -1962.78],
+        title: "Sala de Estar dos Guardas-Marinha",
+        index: 3,
+      }, 
+    ]
+  },
+  {                                                            //6
+    url: "../images/comp/ne-gm-5.jpg",
+    infospots: [
+      {
+        position: [-606.23, -614.82, 4917.16],
+        title: "Sala de Estar dos Guardas-Marinha",
+        index: 7,
+      },
+      {
+        position: [3293.48, -1335.28, 3505.85],
+        title: "Sala de Estar dos Guardas-Marinha",
+        index: 8,
+      },
+      {
+        position: [1549.95, -62.97, -4750.77],
+        title: "Sala de Estar dos Guardas-Marinha",
+        index: 3,
+      },
+      {
+        position: [3729.77, -1707.50, -2844.90],
+        title: "Sala de Estar dos Guardas-Marinha",
+        index: 5,
+      },
+    ]
+  },
+  {                                                            //7
+    url: "../images/comp/ne-gm-6.jpg",
+    infospots: [
+      {
+        position: [1549.95, -62.97, -4750.77],
+        title: "Sala de Estar dos Guardas-Marinha",
+        index: 3,
+      },
+    ]
+  },
+  {                                                            //8
+    url: "../images/comp/ne-gm-7.jpg",
+    infospots: [
+      {
+        position: [1549.95, -62.97, -4750.77],
+        title: "Sala de Estar dos Guardas-Marinha",
+        index: 3,
+      },
+    ]
+  }
+  // Add more panoramas here if needed
+];
+
+export default panoramasData;
