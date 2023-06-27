c4273e9badda3a094535a9c36ef46bed25e3912b
jb-micaloski <jb.micaloski2709@gmail.com>
2023-06-27 08:49:32 -0300

    deleted:    nebrasil_tour/public/images/ne-beg-1.jpg 	modified:   nebrasil_tour/src/App.js

--- nebrasil_tour/src/App.js
+++ nebrasil_tour/src/App.js
@@ -1,4 +1,4 @@
-import React, { lazy, Suspense} from 'react';
+import React, { Fragment, lazy, Suspense, useEffect, useState} from 'react';
 import { BrowserRouter as Router, Route, Routes } from "react-router-dom";
 import './styles/styles.css';
 
@@ -6,28 +6,56 @@ const Pano = lazy(() => import('./components/Pano'));
 const Tour = lazy(() => import('./components/Tour'));
 
 function App() {
+  const [isLoading, setIsLoading] = useState(true);
+
+  useEffect(()=> {
+    const img = ["../images/ne-beg-1.webp"];
+
+    cacheImage(img);
+  },[]);
+
+  const cacheImage = async (srcArray) => {
+    const promises = await srcArray.map((src) => {
+      return new Promise(function (resolve,reject) {
+        const img = new Image();
+        img.src = src;
+        img.onload = resolve();
+        img.onerror = reject();
+      });
+    });
+
+    await Promise.all(promises);
+
+    setIsLoading(false);
+  };
+
   return (
     <div className="App">
-        <Router>
-          <Routes>
-            <Route
-              path="/"
-              element={
-                <Suspense fallback={<div>Loading...</div>}>
-                  <Pano/>
-                </Suspense>
-              }
-            />
-            <Route
-              path="/tour"
-              element={
-                <Suspense fallback={<div>Loading...</div>}>
-                  <Tour />
-                </Suspense>
-              }
-            />
-          </Routes>
-        </Router>
+      {isLoading ?
+        <Suspense fallback={<div>Loading...</div>} />
+        :
+        <Fragment>
+          <Router>
+            <Routes>
+              <Route
+                path="/"
+                element={
+                  <Suspense fallback={<div>Loading...</div>}>
+                    <Pano/>
+                  </Suspense>
+                }
+              />
+              <Route
+                path="/tour"
+                element={
+                  <Suspense fallback={<div>Loading...</div>}>
+                    <Tour />
+                  </Suspense>
+                }
+              />
+            </Routes>
+          </Router>
+        </Fragment>}
     </div>
   );
 }
